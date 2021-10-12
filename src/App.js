7364e3f622bf69d910f17b6da9f9a06a2725b41f
Lavr <avatar_str@list.ru>
2021-10-13 00:15:39 +0500

    Complete DZ-2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { TutorialOne } from './tutorial-1/TutorialOne';
+import { TutorialTwo } from './tutorial-2/TutorialTwo';
 
 export function App() {
   return (
     <div>
       <TutorialOne />
+      <TutorialTwo />
     </div>
   );
 }
---
+++ src/tutorial-2/TutorialTwo.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+export const TutorialTwo = () => {
+  let email = '';
+  let password = '';
+
+  const handleChangeInput = (e) => {
+    const { name, value } = e.target;
+
+    return name === 'email' ? (email = value.trim()) : (password = value.trim());
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (email === '' || password === '') {
+      return alert('sos');
+    }
+
+    console.log({ login: email, password });
+    e.target.reset();
+  };
+
+  return (
+    <form onSubmit={(e) => handleSubmit(e)}>
+      <input type="text" placeholder="e-mail" name="email" onChange={(e) => handleChangeInput(e)} />
+      <input
+        type="password"
+        placeholder="password"
+        name="password"
+        onChange={(e) => handleChangeInput(e)}
+      />
+      <button type="submit">Войти</button>
+    </form>
+  );
+};
