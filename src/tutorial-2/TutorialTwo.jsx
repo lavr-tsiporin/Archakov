b2944aaf9bc0892fda71dcd8db0595e151ee6cad
Lavr <avatar_str@list.ru>
2021-10-16 13:42:42 +0500

    fix for review

--- src/tutorial-2/TutorialTwo.jsx
+++ src/tutorial-2/TutorialTwo.jsx
@@ -4,31 +4,33 @@ export const TutorialTwo = () => {
   let email = '';
   let password = '';
 
-  const handleChangeInput = (e) => {
-    const { name, value } = e.target;
+  const handleChangeInput = (event) => {
+    const { name, value } = event.target;
 
     return name === 'email' ? (email = value.trim()) : (password = value.trim());
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-    if (email === '' || password === '') {
+    if (!email || !password ) {
       return alert('Need fill all inputs');
     }
 
     console.log({ login: email, password });
-    e.target.reset();
+    email=''
+    password=''
+    event.target.reset();
   };
 
   return (
     <form onSubmit={(e) => handleSubmit(e)}>
-      <input type="text" placeholder="e-mail" name="email" onChange={(e) => handleChangeInput(e)} />
+      <input type="text" placeholder="e-mail" name="email" onChange={handleChangeInput} />
       <input
         type="password"
         placeholder="password"
         name="password"
-        onChange={(e) => handleChangeInput(e)}
+        onChange={handleChangeInput}
       />
       <button type="submit">Войти</button>
     </form>
