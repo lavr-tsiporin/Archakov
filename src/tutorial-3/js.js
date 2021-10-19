8ac78816c555963f6ca6fa2cac45cefbe5322c77
Lavr <avatar_str@list.ru>
2021-10-19 14:46:50 +0500

    Create tutorial 3 for review

---
+++ src/tutorial-3/js.js
@@ -0,0 +1,58 @@
+class Todos {
+  constructor(root) {
+    this._todos = JSON.parse(localStorage.getItem('todos')) || [];
+    this._root = root;
+  }
+
+  init(){
+    this.render()
+  }
+
+  render() {
+    this._root.innerHTML = ``
+    localStorage.setItem('todos', JSON.stringify(this._todos));
+    this._todos
+      .map((task, idx) => {
+        const li = document.createElement('li');
+        const btn = document.createElement('button');
+
+        btn.addEventListener(
+          'click',
+          () => {
+            this.handleClickRemove(idx);
+          },
+          { once: true },
+        );
+
+        btn.textContent = 'Удалить';
+        li.textContent = task;
+        li.append(btn);
+
+        return li;
+      })
+      .forEach((tsk) => this._root.append(tsk));
+  }
+
+  handleClickAdd(todo) {
+    if (todo !== '') {
+      this._todos = [...this._todos, todo];
+      this.render();
+    }
+  }
+
+  handleClickRemove(id) {
+    this._todos = [...this._todos].filter((_, idx) => idx !== id);
+    this.render();
+  }
+}
+
+const todos = new Todos(document.querySelector('.root'));
+
+document.addEventListener('DOMContentLoaded', () => {
+  todos.init();
+});
+
+document.querySelector('[name=todos_form]').addEventListener('submit', (e) => {
+  e.preventDefault();
+  todos.handleClickAdd(e.target.todo.value);
+});
